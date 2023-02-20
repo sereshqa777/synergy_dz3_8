90fa368ef3520c602ad7914a1c42c9a097a27aa0
sereshqa777 <116973663+sereshqa777@users.noreply.github.com>
2023-02-20 23:06:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+ 
+  
+int main()
+{
+    int n = 0;
+    printf("Vvedite kolichestvo elementov: ");
+    scanf("%i",&n);
+ 
+    int *number = new int[n];
+ 
+    for (int i = 0; i < n; i++)
+    {
+    	printf("Vvedite element [%i]:",i+1);
+    	scanf("%i",&number[i]);
+    }
+ 
+    int min = number[0];
+    int max = number[0];
+    int sum = 0;
+    float arif;
+    
+ 
+    for (int i = 0; i < n; i++)
+    {
+        if (number[i] < min)
+        {
+            min = number[i];
+        }
+        if (number[i] > max)
+        {
+            max = number[i];
+        }
+        sum += number[i];
+        arif=float(sum)/number[i];
+    }
+ 	
+ 	printf("===========================\n");
+    printf("1) Maximum %i:\n",max);
+    printf("2) Minimum %i:\n",min);
+    printf("3) Summa %i:\n",sum);
+    printf("4) Arithmetic mean: %f\n",arif);
+    printf("===========================");
+ 	
+ 	delete[] number;
+    return 0;
+}
